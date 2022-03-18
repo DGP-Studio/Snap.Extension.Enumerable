4433188ba4e40ef535bddff70c92b76f53292c27
DismissedLight <1686188646@qq.com>
2022-03-18 12:41:08 +0800

    is empty

---
+++ CollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Snap.Extenion.Enumerable
+{
+    public static class CollectionExtensions
+    {
+        public static bool IsEmpty<T>(this ICollection<T>? collection)
+        {
+            return collection is null || collection?.Count == 0;
+        }
+    }
+}
